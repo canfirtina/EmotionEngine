e79dec925329259133e7b46d11b296aab10308fe
Can <cfirtina@ug.bilkent.edu.tr>
2015-02-13 22:09:32 +0200

    SensorManager done

--- src/SensorManager/EEGSensorListener.java
+++ src/SensorManager/EEGSensorListener.java
@@ -1,5 +1,64 @@
 package SensorManager;
 
-public class EEGSensorListener {
+public class EEGSensorListener extends SensorListener{
+
+	public EEGSensorListener(int portNumber) {
+		
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean connect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean disconnect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public byte[] getSensorData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getFrequency() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getPortNumber() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isConnected() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void notifyObservers() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean registerObserver(SensorObserver observer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean removeObserver(SensorObserver observer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
