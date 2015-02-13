07628e66c289ba47fc5ee731edf0eb34580add5b
Mustafa <mustafa.iman@ug.bilkent.edu.tr>
2015-02-13 20:34:34 +0200

    pers data

---
+++ src/PersistentDataManagement/GSRData.java
@@ -0,0 +1,17 @@
+package PersistentDataManagement;
+
+/**
+ * Created by Mustafa on 13.2.2015.
+ */
+public class GSRData implements SensorData {
+    @Override
+    public void fromBinary(byte[] bytes) {
+
+    }
+
+    @Override
+    public byte[] asBinary() {
+        return new byte[0];
+    }
+
+}
