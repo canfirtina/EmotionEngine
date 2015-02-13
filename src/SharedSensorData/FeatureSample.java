c9ebf0f9041d48d9cc20de6d9d1bd0d7b7a51f39
Mustafa <mustafa.iman@ug.bilkent.edu.tr>
2015-02-13 21:20:58 +0200

    pers data

---
+++ src/SharedSensorData/FeatureSample.java
@@ -0,0 +1,22 @@
+package SharedSensorData;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Mustafa on 13.2.2015.
+ */
+public class FeatureSample {
+    private ArrayList<FeatureList> instances;
+
+    public void addFeatureList(FeatureList newList) {
+
+    }
+
+    public byte[] asByteArray() {
+        return new byte[0];
+    }
+
+    public void fromByteArray(byte[] bytes) {
+
+    }
+}
