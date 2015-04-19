1ebb5ac9933d9c44d206af9df95c8faadf654e71
Can <cfirtina@ug.bilkent.edu.tr>
2015-04-19 17:35:20 +0300

    sign up screen

---
+++ src/userinterface/SignUpScreenController.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package userinterface;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author CanFirtina
+ */
+public class SignUpScreenController implements Initializable, PresentedScreen {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @Override
+    public void setPresentingScreen(PresentingController presentingController) {
+        
+    }
+    
+}
