406c04c6eead6db561e2900850e84f79aec0236d
Mustafa <mustafa.iman@ug.bilkent.edu.tr>
2015-04-23 14:44:24 +0300

    COMport listener gets the list of availble ports

--- src/test/CasePortListener.java
+++ src/test/CasePortListener.java
@@ -3,12 +3,16 @@ package test;
 import gnu.io.PortInUseException;
 import sensormanager.COMPortListener;
 
+import java.util.HashMap;
+
 /**
  * Created by Mustafa on 22.4.2015.
  */
 public class CasePortListener {
 
     public void runtest1(){
-        COMPortListener.getConnectedPorts();
+        HashMap<String, Class> m = COMPortListener.getConnectedPorts();
+        for (String key : m.keySet())
+            System.out.println(key + " " + m.get(key));
     }
 }
