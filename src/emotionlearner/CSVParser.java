72542f09def07942aca4335d77657fc8163623e7
Can <cfirtina@ug.bilkent.edu.tr>
2015-04-26 11:02:41 +0300

    featureextractoreeg

--- src/emotionlearner/CSVParser.java
+++ src/emotionlearner/CSVParser.java
@@ -12,66 +12,71 @@ import java.util.Scanner;
  *
  */
 public class CSVParser {
-	public static double[][] read(String src) throws FileNotFoundException {
-		
-		ArrayList<ArrayList<Double>> matrixList = new ArrayList<ArrayList<Double>>();
-		Scanner scanner = new Scanner(new File( src));
-        
-		int currentLine = 0;
-        while(scanner.hasNextLine()){
-        	
+
+    public static double[][] read(String src) throws FileNotFoundException {
+
+        ArrayList<ArrayList<Double>> matrixList = new ArrayList<ArrayList<Double>>();
+        Scanner scanner = new Scanner(new File(src));
+
+        int currentLine = 0;
+        while (scanner.hasNextLine()) {
+
             Scanner scanner2 = new Scanner(scanner.nextLine());
             scanner2.useDelimiter(",");
-            
-            matrixList.add( currentLine, new ArrayList<Double>());
+
+            matrixList.add(currentLine, new ArrayList<Double>());
             int currentColumn = 0;
-            while(scanner2.hasNext())
-            	matrixList.get( currentLine).add( currentColumn++, Double.parseDouble( scanner2.next()));
-            
+            while (scanner2.hasNext()) {
+                matrixList.get(currentLine).add(currentColumn++, Double.parseDouble(scanner2.next()));
+            }
+
             scanner2.close();
             currentLine++;
         }
         scanner.close();
 
         double[][] matrix = new double[matrixList.size()][matrixList.get(0).size()];
-        for( int i = 0 ; i < matrixList.size(); i++)
-        	for( int j = 0; j < matrixList.get(i).size(); j++)
-        		matrix[i][j] = matrixList.get(i).get(j);
+        for (int i = 0; i < matrixList.size(); i++) {
+            for (int j = 0; j < matrixList.get(i).size(); j++) {
+                matrix[i][j] = matrixList.get(i).get(j);
+            }
+        }
         return matrix;
-	}
-	
-	public static void write( String fileName, ArrayList<double[]> list){
-		
-		StringBuilder sb = new StringBuilder();
+    }
 
-		for (double[] arr : list) {
-			for (int i = 0; i < (arr.length-1); i++)
-				sb.append(arr[i] + ",");
-			sb.append(arr[arr.length-1]); //there is no delimiter for the last one
-			sb.append("\n");
-		}
+    public static void write(String fileName, ArrayList<double[]> list) {
 
-		File file = new File( fileName);
-		String content = sb.toString();
- 
-		try (FileOutputStream fop = new FileOutputStream(file)) {
- 
-			// if file doesn't exists, then create it
-			if (!file.exists()) {
-				file.createNewFile();
-			}
- 
-			// get the content in bytes
-			byte[] contentInBytes = content.getBytes();
- 
-			fop.write(contentInBytes);
-			fop.flush();
-			fop.close();
- 
-			System.out.println("Done");
- 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        StringBuilder sb = new StringBuilder();
+
+        for (double[] arr : list) {
+            for (int i = 0; i < (arr.length - 1); i++) {
+                sb.append(arr[i] + ",");
+            }
+            sb.append(arr[arr.length - 1]); //there is no delimiter for the last one
+            sb.append("\n");
+        }
+
+        File file = new File(fileName);
+        String content = sb.toString();
+
+        try (FileOutputStream fop = new FileOutputStream(file)) {
+
+            // if file doesn't exists, then create it
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            // get the content in bytes
+            byte[] contentInBytes = content.getBytes();
+
+            fop.write(contentInBytes);
+            fop.flush();
+            fop.close();
+
+            System.out.println("Done");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/emotionlearner/FeatureExtractorEEG.java
+++ src/emotionlearner/FeatureExtractorEEG.java
@@ -1,6 +1,5 @@
 package emotionlearner;
 
-
 import features.linear.frequency.SpectralAnalysis;
 import features.linear.timeDomain.AutocorrelationCoefficient;
 import features.linear.timeDomain.FanoFactor;
@@ -15,90 +14,92 @@ import features.nonlinear.phaseSpace.LyapunovExponent;
 import features.nonlinear.phaseSpace.StandardDeviationRatio;
 import features.timeFrequency.HaarWaveletStandardDeviation;
 import shared.FeatureList;
+
 /**
  * Extracts features from EEG signals
  *
  */
-public class FeatureExtractorEEG extends FeatureExtractor{
+public class FeatureExtractorEEG extends FeatureExtractor {
 
-	public  FeatureExtractorEEG() {
-		reset();	
-	}
-	
+    public FeatureExtractorEEG() {
+        reset();
+    }
 
-	/**
-	 * Returns Features of the raw EEG data 
-	 * @return
-	 * @throws Exception 
-	 */
-	@Override
-	public FeatureList getFeatures() {
-		final int numFeatures = 20;
-		
-		double[][] rawDataTransposed = new double[rawData.get(0).getData().length][rawData.size()];
-		for(int i=0;i<rawDataTransposed.length;++i)
-			for(int j=0;j<rawDataTransposed[i].length;++j)
-				rawDataTransposed[i][j] = rawData.get(j).getData()[i];
-		
-		double[] res = new double[rawDataTransposed.length * numFeatures];
-		double[] currentFeatures = null;
-		for(int i=0;i<rawDataTransposed.length;++i){
-			currentFeatures = new double[numFeatures];
-			SpectralAnalysis sa = new SpectralAnalysis(rawDataTransposed[i], 256);
-			sa.calculateSpectrumFourier(1);
+    /**
+     * Returns Features of the raw EEG data
+     *
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public FeatureList getFeatures() {
+        final int numFeatures = 20;
 
-			currentFeatures[0] = sa.getAlpha();
-			currentFeatures[1] = sa.getBeta();
-			currentFeatures[2] = sa.getTheta();
-			currentFeatures[3] = sa.getGamma();
-			currentFeatures[4] = sa.getDelta();
-			currentFeatures[5] = sa.getTotalPSD();
-			currentFeatures[6] = AutocorrelationCoefficient.calculateAutocorrelationCoefficient(rawDataTransposed[i]);
-			try {
-				currentFeatures[7] = FanoFactor.calculateFanoFactorFromTimes(rawDataTransposed[i], rawDataTransposed[i].length/ 256);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				currentFeatures[7] = 0;
-				System.err.println("FanoFactor exception");
-			}
-			currentFeatures[8] = MeanOfAbsoluteValuesOfFirstDiffNormalized.calculateMeanOfFirstDiffNormalized(rawDataTransposed[i]);
-			// parametrelerine bak internet geri gelince.
-			currentFeatures[9] = CorrectedConditionalShannonEntropy.calculateCorrectedConditionalShannonEntropy(rawDataTransposed[i], 3, 3);
-			// bunun da bak
-			currentFeatures[10] = new HiguchiDimension(rawDataTransposed[i], 3).getHiguchiDimension(); 
-			currentFeatures[11] = HurstExponent.calculateHurstExponent(rawDataTransposed[i]);
-			try {
-				currentFeatures[12] = AllanFactor.calculateAllanFactorFromTimes(rawDataTransposed[i], rawDataTransposed[i].length);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				currentFeatures[12] = 0;
-				System.err.println("AllanFactor exception");
-			}
-			// buna da bak
-			currentFeatures[13] = CTMSecondOrderDifferencePlot.calculateCTM(rawDataTransposed[i], 100);
-			currentFeatures[14] = LempelZivComplexity.calculateLempelZivComplexity(rawDataTransposed[i]);
-			currentFeatures[15] = 0;//LyapunovExponent.calculateLargestLyapunovExponentStableWolf(rawDataTransposed[i]); TODO
-			currentFeatures[16] = new StandardDeviationRatio(rawDataTransposed[i]).getCSI();
-			currentFeatures[17] = new StandardDeviationRatio(rawDataTransposed[i]).getCVI();
-			currentFeatures[18] = new StandardDeviationRatio(rawDataTransposed[i]).getSD1SD2Ratio();
-			currentFeatures[19] = HaarWaveletStandardDeviation.calculateHaarWaveletSTDEV(rawDataTransposed[i]);
+        double[][] rawDataTransposed = new double[rawData.get(0).getData().length][rawData.size()];
+        for (int i = 0; i < rawDataTransposed.length; ++i) {
+            for (int j = 0; j < rawDataTransposed[i].length; ++j) {
+                rawDataTransposed[i][j] = rawData.get(j).getData()[i];
+            }
+        }
 
-			for (int j = 0; j < numFeatures; j++) 
-				res[i * numFeatures + j] = currentFeatures[j];
-		}
-		
-		return new FeatureList(selectFeatures(res));
-	}
+        double[] res = new double[rawDataTransposed.length * numFeatures];
+        double[] currentFeatures = null;
+        for (int i = 0; i < rawDataTransposed.length; ++i) {
+            currentFeatures = new double[numFeatures];
+            SpectralAnalysis sa = new SpectralAnalysis(rawDataTransposed[i], 256);
+            sa.calculateSpectrumFourier(1);
 
+            currentFeatures[0] = sa.getAlpha();
+            currentFeatures[1] = sa.getBeta();
+            currentFeatures[2] = sa.getTheta();
+            currentFeatures[3] = sa.getGamma();
+            currentFeatures[4] = sa.getDelta();
+            currentFeatures[5] = sa.getTotalPSD();
 
-	/**
-	 * To be continued
-	 */
-	@Override
-	protected double[] selectFeatures(double[] features) {
-		return features;
-	}
-	
-	
-	
+            currentFeatures[6] = AutocorrelationCoefficient.calculateAutocorrelationCoefficient(rawDataTransposed[i]);
+            try {
+                currentFeatures[7] = FanoFactor.calculateFanoFactorFromTimes(rawDataTransposed[i], rawDataTransposed[i].length / 256);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                currentFeatures[7] = 0;
+                System.err.println("FanoFactor exception");
+            }
+            currentFeatures[8] = MeanOfAbsoluteValuesOfFirstDiffNormalized.calculateMeanOfFirstDiffNormalized(rawDataTransposed[i]);
+            // parametrelerine bak internet geri gelince.
+            currentFeatures[9] = CorrectedConditionalShannonEntropy.calculateCorrectedConditionalShannonEntropy(rawDataTransposed[i], 3, 3);
+            // bunun da bak
+            currentFeatures[10] = new HiguchiDimension(rawDataTransposed[i], 3).getHiguchiDimension();
+            currentFeatures[11] = HurstExponent.calculateHurstExponent(rawDataTransposed[i]);
+            try {
+                currentFeatures[12] = AllanFactor.calculateAllanFactorFromTimes(rawDataTransposed[i], rawDataTransposed[i].length);
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                currentFeatures[12] = 0;
+                System.err.println("AllanFactor exception");
+            }
+            // buna da bak
+            currentFeatures[13] = CTMSecondOrderDifferencePlot.calculateCTM(rawDataTransposed[i], 100);
+            currentFeatures[14] = LempelZivComplexity.calculateLempelZivComplexity(rawDataTransposed[i]);
+            currentFeatures[15] = 0;//LyapunovExponent.calculateLargestLyapunovExponentStableWolf(rawDataTransposed[i]); TODO
+            currentFeatures[16] = new StandardDeviationRatio(rawDataTransposed[i]).getCSI();
+            currentFeatures[17] = new StandardDeviationRatio(rawDataTransposed[i]).getCVI();
+            currentFeatures[18] = new StandardDeviationRatio(rawDataTransposed[i]).getSD1SD2Ratio();
+            currentFeatures[19] = HaarWaveletStandardDeviation.calculateHaarWaveletSTDEV(rawDataTransposed[i]);
+
+            for (int j = 0; j < numFeatures; j++) {
+                res[i * numFeatures + j] = currentFeatures[j];
+            }
+        }
+
+        return new FeatureList(selectFeatures(res));
+    }
+
+    /**
+     * To be continued
+     */
+    @Override
+    protected double[] selectFeatures(double[] features) {
+        return features;
+    }
+
 }
